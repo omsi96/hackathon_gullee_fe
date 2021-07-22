735f08b724b49e758ddd26a6410b63ca36d9378b
Rahaf Alenezi <irahaf@live.it>
2021-07-23 01:40:21 +0300

    a green wave added

--- gulle/src/components/Home/index.js
+++ gulle/src/components/Home/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const index = () => {
-    return (
-        <div>
-           <h2>Home</h2> 
-        </div>
-    );
-};
+  return (
+    <div>
+      <h2>Home</h2>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 1440 320'
+        style={{ position: 'fixed', bottom: '0px' }}>
+        <path
+          fill='#87C772'
+          fill-opacity='1'
+          d='M0,96L120,80C240,64,480,32,720,37.3C960,43,1200,85,1320,106.7L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z'></path>
+      </svg>
+    </div>
+  )
+}
 
-export default index;
+export default index
